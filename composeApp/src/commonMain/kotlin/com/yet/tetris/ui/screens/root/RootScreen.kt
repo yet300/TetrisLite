4b485876a2f82b3c4a31d1a9c3812cfd84e6c8c7
yet300 <ghg00rus300@gmail.com>
2025-10-08 15:49:48 +0400

    feat: Add RootScreen composable for navigation
    Introduces the `RootScreen` composable, which manages the UI display of the root component's child stack using Decompose's `Children` function.
    
    - Subscribes to the `childStack` from `RootComponent`.
    - Renders `HomeScreen` or `GameScreen` based on the active child instance (`RootComponent.Child.Home` or `RootComponent.Child.Game`).

---
+++ composeApp/src/commonMain/kotlin/com/yet/tetris/ui/screens/root/RootScreen.kt
@@ -0,0 +1,24 @@
+package com.yet.tetris.ui.screens.root
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+
+import com.arkivanov.decompose.extensions.compose.stack.Children
+import com.arkivanov.decompose.extensions.compose.subscribeAsState
+import com.yet.tetris.feature.root.RootComponent
+import com.yet.tetris.ui.screens.game.GameScreen
+import com.yet.tetris.ui.screens.home.HomeScreen
+
+@Composable
+fun RootScreen(component: RootComponent) {
+    val childStack by component.childStack.subscribeAsState()
+
+    Children(
+        stack = childStack,
+    ) { child ->
+        when (val instance = child.instance) {
+            is RootComponent.Child.Home -> HomeScreen(instance.component)
+            is RootComponent.Child.Game -> GameScreen(instance.component)
+        }
+    }
+}
