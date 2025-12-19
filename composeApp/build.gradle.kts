1a70c44daf02126ebb8a87db357008613a80ec5a
yet300 <ghg00rus300@gmail.com>
2025-12-19 21:33:40 +0400

    build(release): Bump version to 1.1  tag v1.0.1

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -93,8 +93,8 @@ android {
             libs.versions.android.targetSdk
                 .get()
                 .toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     packaging {
