1cac3b289ae77acd9e06dfc5af0b6131e7cc58d7
yet300 <ghg00rus300@gmail.com>
2026-02-27 15:13:03 +0400

    refactor(core): Use DefaultStoreFactory for MVIKotlin

--- core/common/src/commonMain/kotlin/com/app/common/provideStoreFactory.kt
+++ core/common/src/commonMain/kotlin/com/app/common/provideStoreFactory.kt
@@ -1,9 +1,8 @@
 package com.app.common
 
 import com.arkivanov.mvikotlin.core.store.StoreFactory
-import com.arkivanov.mvikotlin.logging.store.LoggingStoreFactory
-import com.arkivanov.mvikotlin.timetravel.store.TimeTravelStoreFactory
+import com.arkivanov.mvikotlin.main.store.DefaultStoreFactory
 import org.koin.core.annotation.Single
 
 @Single
-fun provideStoreFactory(): StoreFactory = LoggingStoreFactory(TimeTravelStoreFactory())
+fun provideStoreFactory(): StoreFactory = DefaultStoreFactory()
